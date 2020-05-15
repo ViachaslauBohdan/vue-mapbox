802f642bcaf9c01a8b2bbc13c4a2ebbab600de78
Viachaslau Bohdan <viachaslau.bohdan@onemeter.com>
2020-05-15 13:02:35 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <div @click="loadMap()">
+    <button>Click me</button>
   </div>
+  <div style="width:1200px;height:1200px" v-if="showMap">
+    <MglMap :accessToken="accessToken" :mapStyle="mapStyle" :mapboxGl="mapbox-gl" />
+  </div>
+</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+// import Mapbox from "mapbox-gl";
+import { MglMap } from "vue-mapbox";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    MglMap
+  },
+  data() {
+    return {
+      accessToken: 'pk.eyJ1IjoiYXBhZGV2IiwiYSI6ImNrYTJnZWZjeTBhNHIzZHAwMjl3OGNtZmsifQ.fVVScoKaYNsms9DbXaKNpw', // your access token. Needed if you using Mapbox maps
+      mapStyle: 'mapbox://styles/mapbox/streets-v11', // your map style
+      showMap: false
+    };
+  },
+  methods: {
+    loadMap() {
+      this.showMap = true
+    }
+  },
+
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    // this.mapbox = Mapbox;
+  }
+};
+</script>
